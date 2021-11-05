0439ce84017a22cfdc84eb64a193c966d824016e
Sheheryar Waseem <sherishaikh6@gmail.com>
2021-11-05 11:09:25 +0500

    updated

--- server.mjs
+++ server.mjs
@@ -1,10 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import morgan from 'morgan';
+import cors from 'cors';
 
-
-const = app = express();
+const app = express()
 const port = process.env.PORT || 3000
+
 
 let users = [];
 app.use(cors())
@@ -12,75 +12,79 @@ app.use(express.json())
 app.use(morgan('short'))
 
 app.use((req, res, next) => {
-    console.log("A request came", req.body);
-    next()
+  console.log("a request came", req.body);
+  next()
 })
 
 app.get('/users', (req, res) => {
-    res.send(users)
+  res.send(users)
 })
+app.get('/user/:id', (req, res) => {
 
-app.get('/users/:id', (req, res)=> {
-    if(users[req.params.id]){
+  if (users[req.params.id]) {
     res.send(users[req.params.id])
-    }
-    else{
-        res.send("users not found")
-    }
-})
+  } else {
+    res.send("user not found");
+  }
 
+})
 app.post('/user', (req, res) => {
-    if(!req.body.name || !req.body.email || !req.body.address){
-        res.status(400).send("invalid data")
-    }
-    else{
-        users.push({
-            name: req.body.name,
-            email: req.body.email,
-            address: req.body.address
-        })
 
-        res.send("Users Created");
+  if (!req.body.name || !req.body.email || !req.body.address) {
+    res.status(400).send("invalid data");
+  } else {
+    users.push({
+      name: req.body.name,
+      email: req.body.email,
+      address: req.body.address
+    })
+
+    res.send("users created");
+  }
+})
+app.put('/user/:id', (req, res) => {
+
+  if (users[req.params.id]) {
+
+    if (req.body.name) {
+      users[req.params.id].name = req.body.name
     }
+    if (req.body.email) {
+      users[req.params.id].email = req.body.email
+    }
+    if (req.body.address) {
+      users[req.params.id].address = req.body.address
+    }
+
+    res.send(users[req.params.id])
+
+  } else {
+    res.send("user not found");
+  }
+
+
 
 })
 
-app.put('/user/:id',(req, res) =>{
-    if(users[req.params.id]){
-        if(req.body.name){
-            users[req.params.id].name = req.body.name
-        }
-        if(req.body.email){
-            users[req.params.id].email = req.body.email
-        }
-        if(req.body.address){
-            users[req.params.id].email = req.body.address
-        }
-        res.send(users[req.params.id])
-    }
-    else{
-        res.send("user not found");
-    }
+app.delete('/user/:id', (req, res) => {
+
+  if (users[req.params.id]) {
+
+    users[req.params.id] = {};
+    res.send("user deleted");
+
+  } else {
+    res.send("user not found");
+  }
 })
 
-app.delete('/user/:id', (req, res) =>{
-
-    if (users[req.params.id]){
-        user[req.params.id] = {};
-        res.send("User Deleted")
-    }
-    else{
-        res.send("user not found")
-    }
+app.get('/home', (req, res) => {
+  res.send('here is your home')
+})
+app.get('/', (req, res) => {
+  res.send('Hi I am a hello world Server program')
 })
 
-app.get('/home',(req, res)=>{
-    res.send('Welcome to Sheri home')
-})
-app.get('/',(req, res)=>{
-    res.send('Hello Iam Sheri Server')
-})
-app.listen(port, ()=>{
-    console.log(`Example app listening at  http://localhost:${port}`);
-
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
